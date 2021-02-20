d154287fbd3a3a0a7b29868df7831b38a8b284fb
Mateus <mateusp.1996@gmail.com>
2021-02-20 12:31:41 -0300

    commit initial

---
+++ LojaVirtual/LojaVirtual/Migrations/20210204211134_BancoInitial.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaVirtual.Migrations
+{
+    public partial class BancoInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    Slug = table.Column<string>(nullable: false),
+                    CategoriaPaiId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categorias_Categorias_CategoriaPaiId",
+                        column: x => x.CategoriaPaiId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    Nascimento = table.Column<DateTime>(nullable: false),
+                    Sexo = table.Column<string>(nullable: false),
+                    CPF = table.Column<string>(nullable: false),
+                    Telefone = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    CEP = table.Column<string>(maxLength: 10, nullable: false),
+                    Estado = table.Column<string>(nullable: false),
+                    Cidade = table.Column<string>(nullable: false),
+                    Bairro = table.Column<string>(nullable: false),
+                    Endereco = table.Column<string>(nullable: false),
+                    Complemento = table.Column<string>(nullable: false),
+                    Numero = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: false),
+                    Situacao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Colaboradores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Senha = table.Column<string>(nullable: false),
+                    Tipo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colaboradores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewsletterEmails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsletterEmails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    Descricao = table.Column<string>(nullable: false),
+                    Valor = table.Column<decimal>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false),
+                    Peso = table.Column<double>(nullable: false),
+                    Largura = table.Column<int>(nullable: false),
+                    Altura = table.Column<int>(nullable: false),
+                    Comprimento = table.Column<int>(nullable: false),
+                    CategoriaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnderecosEntrega",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    CEP = table.Column<string>(maxLength: 10, nullable: false),
+                    Estado = table.Column<string>(nullable: false),
+                    Cidade = table.Column<string>(nullable: false),
+                    Bairro = table.Column<string>(nullable: false),
+                    Endereco = table.Column<string>(nullable: false),
+                    Complemento = table.Column<string>(nullable: false),
+                    Numero = table.Column<string>(nullable: true),
+                    ClienteId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnderecosEntrega", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EnderecosEntrega_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Imagens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Caminho = table.Column<string>(nullable: true),
+                    ProdutoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imagens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Imagens_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categorias_CategoriaPaiId",
+                table: "Categorias",
+                column: "CategoriaPaiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnderecosEntrega_ClienteId",
+                table: "EnderecosEntrega",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imagens_ProdutoId",
+                table: "Imagens",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Colaboradores");
+
+            migrationBuilder.DropTable(
+                name: "EnderecosEntrega");
+
+            migrationBuilder.DropTable(
+                name: "Imagens");
+
+            migrationBuilder.DropTable(
+                name: "NewsletterEmails");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
---
+++ LojaVirtual/LojaVirtual/Migrations/20210204211417_Categorias.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaVirtual.Migrations
+{
+    public partial class Categorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
